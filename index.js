e3e3d61780c7505f81cee294a927697eec90852c
Tae Sandoval Murgan <tae@mylodon.cl>
2015-09-21 02:15:59 -0300

    Put the yield statement after the redirect

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = function lowercaseUrl() {
 	return function *(next) {
 		var url = this.request.url
-		yield next
 		if (/[A-Z]/.test(url)) {
 			this.response.status = 301
 			this.response.redirect(url.toLowerCase());
+			return
 		}
+		yield next
 	}
 }()
