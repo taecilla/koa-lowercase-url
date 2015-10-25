7f4776fa6d9255b4a91ba8728cfc632779274a63
Tae Sandoval <tssm@users.noreply.github.com>
2015-10-25 16:08:09 -0300

    Handle host and ignore query string

--- index.js
+++ index.js
@@ -2,10 +2,20 @@
 
 module.exports = function lowercaseUrl() {
 	return function *(next) {
-		var url = this.request.url
+		const href = this.request.href
+
+		let url = href,
+			queryString = ''
+
+		const queryStart = href.indexOf('?')
+		if (queryStart > 0) {
+			url = href.slice(0, queryStart)
+			queryString = href.slice(queryStart)
+		}
+
 		if (/[A-Z]/.test(url)) {
 			this.response.status = 301
-			this.response.redirect(url.toLowerCase());
+			this.response.redirect(url.toLowerCase() + queryString)
 			return
 		}
 		yield next
